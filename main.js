fa22922a04a8dc3b7890bf3e6c209181262ae045
gregorious-j <40620725+gregorious-j@users.noreply.github.com>
2018-11-06 14:33:19 -0500

    added y-int and color graphs

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 
 
-let Tx = 400;
-let Ty = 400;
+let offsetX = 400;
+let offsetY = 400;
 let mapFactor = 1;
-var zoom, a, b, c, xydisplay;
-
+var zoom, zoomReset, a, b, c, xydisplay;
 
 
 function setup() {
@@ -22,9 +21,12 @@ function setup() {
 
 function draw() {
     
+
+    
     background(255);
+    
     strokeWeight(1);
-    fill(0);
+    fill('blue');
     textFont('Helvetica');
     textSize(20);
     stroke('blue');
@@ -32,18 +34,35 @@ function draw() {
     
     text('equation y = ' + a.value() + 'x' + '^2' + ' + ' +  b.value()  + 'x ' + '+ ' + c.value(), width-250, 30);
     //console.log(zoom.value());    
+    if(graph2.checked() == true) {
+        stroke('red');
+        fill('red');
+        text('equation y = ' + a2.value() + 'x' + '^2' + ' + ' +  b2.value()  + 'x ' + '+ ' + c2.value(), width-250, 55);
+    }
     
     noFill();
-    translate(Tx, Ty);
+    translate(offsetX, offsetY);
     drawGrid();
     drawGraphs();
     coolMouse();
+    
+    
+    zoomP.html('Zoom: ' + zoom.value());
+    //zoomReset.mouseIsPressed(zoom.value(100));
+    
 
     
 }
+function doubleClicked() {
+    if (mouseX <= width && mouseY >= 0) {
+    offsetX = ((400 - mouseX) + offsetX);
+    offsetY = ((400 - mouseY) + offsetY);
+    }
+}
 
 function drawGraphs() {
-    stroke(0);
+    
+    stroke('blue');
     strokeWeight(2);
     beginShape();
     for (x = -width; x < width; x++) {
@@ -53,23 +72,53 @@ function drawGraphs() {
     }
     endShape();
     if (graph2.checked() == true) {
+        stroke('red');
         beginShape();
         for (x = -width; x < width; x++) {
             noFill();
             vertex(x, Calc(x, a2.value()/zoom.value(), b2.value(), c2.value()*zoom.value()));
+            
+           
             //console.log(Calc(x));
         }
         endShape();
+        var zeroY2 = Calc(0, a2.value()/zoom.value(), b2.value(), c2.value()*zoom.value());
+            stroke(0, 255, 10);
+            strokeWeight(5);
+            point(0, zeroY2);
+            strokeWeight(1);
+            stroke(100);
+            fill(100);
+            text('y-int ' + -zeroY2/zoom.value(), 5, zeroY2-10);
+
+
     }
+    var zeroY = Calc(0, a.value()/zoom.value(), b.value(), c.value()*zoom.value());
+    
+    strokeWeight(5);
+    stroke(0, 255, 10);
+    point(0, zeroY);
+    strokeWeight(1);
+    stroke(100);
+    fill(100);
+    
+    text('y-int ' + -zeroY/zoom.value(), 5, zeroY-10);
 }
 
 
 //Creates HTML Elements
 
 function setupInputs1() {
-    zoom = createSlider(1, 100, 2);
-    zoom.position(20,80);
+    zoom = createSlider(1, 400, 100, 1);
+    zoom.position(20,90);
     zoom.style('width', '200px');
+    zoomP = createP('');
+    zoomP.position(25, 95);
+    
+    zoomReset = createButton('Reset');
+    zoomReset.position(230, 90);
+    zoomReset.mousePressed(resetZoom());
+    
     
     var off = 200;
 
@@ -96,38 +145,46 @@ function setupInputs1() {
     //mode = createCheckbox('Quadratic', false);
     //mode.position(1200, 420);
 
-    xydisplay = createCheckbox('Show coordinates next to pointer', true);
+    xydisplay = createCheckbox('Show coordinates next to pointer', false);
     xydisplay.position(1175-200, 440+400);
   
 }
 
 function setupInputs2() {
 
-    var off2 = 350;
-    atext2 = createP('a:');
-    atext2.position(1180-200, off2+40);
-
-    btext2 = createP('b (slope if a=0):');
-    btext2.position(1180-291, off2+20);
-
-    ctext2 = createP('c:');
-    ctext2.position(1180-200, off2);
-
-    a2 = createInput('0', 'number');
-    a2.position(1200-200, off2+40);
-    a2.style('100px');
-
-    b2 = createInput('0', 'number');
-    b2.position(1200-200, off2+20);
-
-    c2 = createInput('0', 'number');
-    c2.position(1200-200, off2);
-
     graph2 = createCheckbox('Graph 2nd function', false);
-    graph2.position(1175-200, 440+100);
+    graph2.position(1175-200, 300);
+
+        var off2 = 350;
+        atext2 = createP('a:');
+        atext2.position(1180-200, off2+25);
+    
+        btext2 = createP('b (slope if a=0):');
+        btext2.position(1180-291, off2+5);
+    
+        ctext2 = createP('c:');
+        ctext2.position(1180-200, off2-15);
+    
+        a2 = createInput('0', 'number');
+        a2.position(1200-200, off2+40);
+        a2.style('100px');
+    
+        b2 = createInput('0', 'number');
+        b2.position(1200-200, off2+20);
+    
+        c2 = createInput('0', 'number');
+        c2.position(1200-200, off2);
+    
+   
 
 
 } 
+
+function resetZoom() {
+    if (zoom.value() != 100) {
+        zoom.value(100);
+    }
+}
 
 //Calculates equation
 
@@ -143,24 +200,25 @@ function Calc(x, av, bv, cv) {
 //Cross and (x, y) coordinate
 
 function coolMouse() {
-    var textX = roundNum((mouseX - Tx)/zoom.value(), 1);
-    var textY = roundNum((mouseY - Ty)/zoom.value(), 1);
+    var textX = roundNum((mouseX - offsetX)/zoom.value(), 1);
+    var textY = roundNum((mouseY - offsetY)/zoom.value(), 1);
 
-    fill('red');
+    fill(0);
     if (xydisplay.checked() == true) {
         strokeWeight(1);
-        stroke('red');
+        stroke(0);
         cursor(CROSS);
-        translate(-Tx, -Ty);
+        translate(-offsetX, -offsetY);
         text('('+ textX + ', ' + -textY + ')', mouseX-80, mouseY-20);
 
     } else {
         cursor();
-        translate(-Tx, -Ty);
+        translate(-offsetX, -offsetY);
         strokeWeight(1);
-        stroke('red');
-        text('('+ textX + ', ' + -textY + ')', 20, 40);
+        stroke(0);
+        text('('+ textX + ', ' + -textY + ')', 20, 80);
     }
+    
     
     //if (mouseIsClicked) {
     //point(mouseX, mouseY);
